32da9a97054a7c31ea157c6cde0f6e520e507302
Samabaghdady <samaalbaghdady90@gmail.com>
2025-06-14 16:56:33 +0300

    Add files via upload

---
+++ config/sslConfig.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const sslConfig = {
+  key: fs.readFileSync(path.join(__dirname, '../ssl/private.key')),
+  cert: fs.readFileSync(path.join(__dirname, '../ssl/certificate.crt')),
+  allowHTTP1: true,
+  minVersion: 'TLSv1.2',
+  ciphers: [
+    'ECDHE-ECDSA-AES128-GCM-SHA256',
+    'ECDHE-RSA-AES128-GCM-SHA256',
+    'ECDHE-ECDSA-AES256-GCM-SHA384',
+    'ECDHE-RSA-AES256-GCM-SHA384',
+    'ECDHE-ECDSA-CHACHA20-POLY1305',
+    'ECDHE-RSA-CHACHA20-POLY1305',
+    'DHE-RSA-AES128-GCM-SHA256',
+    'DHE-RSA-AES256-GCM-SHA384'
+  ].join(':'),
+  ocsp: true,
+  sessionTimeout: 300000, // 5 minutes
+  secureOptions: require('constants').SSL_OP_NO_SSLv2 | require('constants').SSL_OP_NO_SSLv3 | require('constants').SSL_OP_NO_TLSv1 | require('constants').SSL_OP_NO_TLSv1_1
+};
+
+module.exports = sslConfig; 
